9c2a053ce16f4737fe059f04799da5337174b6f1
Claudio Barlassina <claudio_barlassina_ar@hotmail.com>
2026-05-14 15:14:26 -0300

    confirmar

--- BackEnd_Mongo_Cloud/ServicioEmail/email/email.services.js
+++ BackEnd_Mongo_Cloud/ServicioEmail/email/email.services.js
@@ -1,25 +1,41 @@
 import nodemailer from "nodemailer"
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 })
 
-export const sendEmail = async ({ to, subject, html }) => {
+export const sendEmail = async ({
+  to,
+  subject,
+  html
+}) => {
+
   try {
+
+    console.log("📨 Enviando email a:");
+
+    console.log(to);
+
+    const info =
     await transporter.sendMail({
       from: `"Star Accesorios" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       html
-    })
+    });
+
+    console.log("✅ Email enviado");
+
+    console.log(info.response);
+
   } catch (error) {
-    console.log("Error enviando email", error)
+
+    console.log("❌ ERROR EMAIL", error);
+
+    console.log(error);
   }
 }
