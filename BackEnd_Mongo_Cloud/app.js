1f6e1a96675b759e92cdb9eefc95311b8e070dd0
Claudio Barlassina <claudio_barlassina_ar@hotmail.com>
2026-05-14 14:45:38 -0300

    server

--- BackEnd_Mongo_Cloud/app.js
+++ BackEnd_Mongo_Cloud/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors"
-import helmet from "helmet"
-import rateLimit from "express-rate-limit"
+// import helmet from "helmet"
+// import rateLimit from "express-rate-limit"
 import dotenv from "dotenv";
 import { connectDB } from "./config/mongo.js";
 import productsRoutes from "./routes/products.routes.js";
@@ -9,20 +9,20 @@ import ordersRoutes from "./routes/orders.routes.js";
 
 dotenv.config();
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-  message: { error: "Demasiadas solicitudes, intente de nuevo más tarde" },
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 100,
+//   standardHeaders: true,
+//   legacyHeaders: false,
+//   message: { error: "Demasiadas solicitudes, intente de nuevo más tarde" },
+// });
 
 const app = express();
 app.set("trust proxy", 1);
 app.use(helmet())
 app.use(cors())
-app.use(express.json({ limit: "1mb" }))
-app.use(limiter)
+app.use(express.json())
+// app.use(limiter)
 
 app.use("/api/pedidos", ordersRoutes);
 app.use("/api/products", productsRoutes);
