cc13f196e347b43b52fd670adf23451a48fd9dc9
Claudio Barlassina <claudio_barlassina_ar@hotmail.com>
2026-05-15 01:17:36 -0300

    resend

--- BackEnd_Mongo_Cloud/ServicioEmail/email/email.services.js
+++ BackEnd_Mongo_Cloud/ServicioEmail/email/email.services.js
@@ -3,20 +3,29 @@ import { Resend } from "resend"
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendEmail = async ({ to, subject, html }) => {
+
   try {
+
     console.log("📨 Enviando email a:", to)
+
     const { error } = await resend.emails.send({
-      from: `Star Accesorios <${process.env.EMAIL_FROM}>`,
+      from: "Star Accesorios <onboarding@resend.dev>",
       to,
       subject,
       html,
     })
+
     if (error) {
       console.error("❌ Error Resend:", error)
       return
     }
+
     console.log("✅ Email enviado a:", to)
+
   } catch (error) {
+
     console.error("❌ ERROR EMAIL:", error.message)
+
   }
+
 }
--- BackEnd_Mongo_Cloud/ServicioEmail/email/pedidoEmail.services.js
+++ BackEnd_Mongo_Cloud/ServicioEmail/email/pedidoEmail.services.js
@@ -1,33 +1,24 @@
-import { sendEmail } from "./email.services.js";
-import { clienteTemplate } from "../email/template/cliente.template.js";
-import { adminTemplate } from "../email/template/admin.template.js";
+export const enviarPedidoEmails = async (pedido) => {
 
+  console.log("📦 Pedido recibido:")
+  console.log(pedido)
 
-export const enviarPedidoEmails =
-async (pedido) => {
+  console.log("📩 Email cliente:")
+  console.log(pedido.cliente.email)
 
-  console.log("📦 Pedido recibido:");
+  console.log("🔔 Email admin:")
+  console.log(process.env.ADMIN_EMAIL)
 
-  console.log(pedido);
-
-  console.log("📩 Email cliente:");
-
-  console.log(pedido.cliente.email);
-
-  console.log("🔔 Email admin:");
-
-  console.log(process.env.EMAIL_USER);
-
-  // 📩 Cliente
+  // Cliente
   await sendEmail({
     to: pedido.cliente.email,
     subject: "Confirmación de compra",
     html: clienteTemplate(pedido)
   })
 
-  // 🔔 Admin
+  // Admin
   await sendEmail({
-    to: process.env.EMAIL_USER,
+    to: process.env.ADMIN_EMAIL,
     subject: "Nuevo pedido recibido",
     html: adminTemplate(pedido)
   })
