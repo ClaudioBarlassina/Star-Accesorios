a8501926ad77e72c9008a0deb8c1f458b5c27a35
Claudio Barlassina <claudio_barlassina_ar@hotmail.com>
2026-08-18 01:29:13 -0300

    instalar chrome

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -4,6 +4,12 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom"
 import App from './App.jsx'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => {});
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
